a5e1bdfb7cbd9d1c4c4599ecb0fa3eee6b37eb43
Brad Saund <brad.saund@gmail.com>
2016-10-24 16:26:10 -0400

    moveAroundRealBoeign part, still in testing phase

--- Scenarios/Tactile/moveAroundRealPart.h
+++ Scenarios/Tactile/moveAroundRealPart.h
@@ -39,7 +39,7 @@ static void moveStartToSide()
 {
   std::cout << "Moving to: Side Face" << std::endl;
   moveToStartPose();
-
+ 
   double midAngles[7] = {0, -.785, 0, 1.3708, 0, .985, -2.3};
   TLU::moveToAngles(midAngles);
 }
@@ -61,66 +61,5 @@ static void moveFrontRightToTop()
   moveToStartPose();
 }
 
-static void moveTopToSide()
-{
-  std::cout << "Moving to: Side Face" << std::endl;
-  moveToStartPose();
-
-  double midAngles[7] = {0.0, -.9, 0, 1.7, 0, .81, -1.57};
-  TLU::moveToAngles(midAngles);
-
-  TLU::moveToPose(Pose(0.636, 0, .523, 1.5708, 0, -1.0));
-  TLU::moveToPose(Pose(0.636, 0, .323, 1.5708, 0, -1.0));
-
-  // TLU::moveToAngles(midAngles);
-  // TLU::moveToPose(Pose(.69, .14, .65, 1.58, -1.23, 2.724));
-  // TLU::moveToPose(Pose(.71, .13, .4, 1.58, -1.23, 2.724));
-
-}
-
-static void moveSideToTop()
-{
-  std::cout << "Moving from side to top" << std::endl;
-  // TLU::moveToPose(Pose(.71, .13, .4, 1.58, -1.23, 2.724));
-  // std::cout << "Moving from side to stop stage 2" << std::endl;
-  // TLU::moveToPose(Pose(.69, .14, .65, 1.58, -1.23, 2.724));
-
-  // double midAngles[7] = {0.55, -.9, 0, 1.7, 0, .81, -1.5};
-  // TLU::moveToAngles(midAngles);
-
-  TLU::moveToPose(Pose(0.636, 0, .34, 1.5708, 0, -1.0));
-  TLU::moveToPose(Pose(0.636, 0, .523, 1.5708, 0, -1.0));
-  double midAngles[7] = {0.0, -.9, 0, 1.7, 0, .81, -1.57};
-
-  TLU::moveToAngles(midAngles);
-  
-
-
-  moveToStartPose();
-}
-
-static void moveStartToTop()
-{
-  std::cout << "Moving to: Top Face" << std::endl;
-  moveToStartPose();
-
-  double midAngles[7] = {0.0, -.8, 0, 1.56, -1.59, 1.55, -1.65};
-  TLU::moveToAngles(midAngles);
-
-  TLU::moveToPose(Pose(0.74, -.32, .77, 0, 0, 0));
-  TLU::moveToPose(Pose(0.74, -.32, .55, 0,0,0));
-
-}
-
-static void moveTopToStart()
-{
-  std::cout << "Moving from top to start" << std::endl;
-  TLU::moveToPose(Pose(0.74, -.32, .55, 0,0,0));
-  TLU::moveToPose(Pose(0.74, -.32, .77, 0, 0, 0));
-
-  double midAngles[7] = {0.0, -.8, 0, 1.56, -1.59, 1.55, -1.65};
-  TLU::moveToAngles(midAngles);
-  moveToStartPose();
-}
 
 #endif
